4db0cb0baf83a76eef91124c2fe6f6328ef5d192
zamasu <zxm273293@gmail.com>
2025-11-11 13:39:22 +0300

    Add files via upload

---
+++ dist/slashCommands/config/setPrefix.js
@@ -0,0 +1,43 @@
+import { SlashCommandStringOption } from "discord.js";
+import { SlashCommand } from "../../lib/quest/handler/slashCommand.js";
+import GuildDocument from "../../entities/guildSettings.js";
+import { EmbedBuilder } from "../../lib/quest/handler/embedBuilder.js";
+export default class setprefix extends SlashCommand {
+    constructor() {
+        super(...arguments);
+        this.name = "setprefix";
+        this.description = "Set the prefix for the bot in this server";
+        this.options = [
+            new SlashCommandStringOption().setName("prefix")
+                .setDescription("The new prefix for the bot")
+                .setMaxLength(2)
+                .setMinLength(1)
+                .setRequired(true)
+        ];
+        this.cooldown = "1m";
+        this.allowedRoles = [];
+        this.allowedServers = [];
+        this.allowedUsers = [];
+        this.allowedChannels = [];
+        this.permissions = ["Administrator"];
+        this.bot_permissions = [];
+        this.flags = ["onlyGuild", "ephemeral"];
+    }
+    async execute({ interaction, client, i18n, lang }) {
+        const guild = interaction.guild;
+        const GuildDocumentRepo = this.appDataSource.getRepo(GuildDocument);
+        let guildSettings = client.guildSettings.get(guild.id);
+        if (!guildSettings) {
+            let doc = await GuildDocumentRepo.findOneBy({ guildId: guild.id }) ?? GuildDocumentRepo.create({ guildId: guild.id });
+            await GuildDocumentRepo.save(doc);
+            client.guildSettings.set(guild.id, guildSettings);
+            guildSettings = client.guildSettings.get(guild.id);
+        }
+        const newPrefix = interaction.options.getString("prefix");
+        guildSettings.prefix = newPrefix;
+        await GuildDocumentRepo.save(guildSettings);
+        client.guildSettings.set(guild.id, guildSettings);
+        const embed = new EmbedBuilder().setDescription(i18n.t("setprefix.prefixSetMessage", { prefix: newPrefix }));
+        await interaction.editReply({ embeds: [embed] });
+    }
+}
